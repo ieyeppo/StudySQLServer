7922b5920adaf413e3ace3c8a4bc7267997f8e65
ieyeppo <ieyeppo@gmail.com>
2021-02-05 17:14:53 +0900

    sql 연습

--- 03_practice/select_query.sql
+++ 03_practice/select_query.sql
@@ -1,1 +1,19 @@
-select * from memberTBL;
+-- select * from memberTBL;
+
+-- select memberID, memberName from memberTBL
+-- where memberID like 'H%';
+
+use BikeStores;
+
+select * from sales.customers;
+
+select * from production.products;
+
+-- DB 조회
+exec sp_helpdb;
+
+-- 테이블 조회
+exec sp_tables @table_type = "'table'";
+
+exec sp_columns @table_name = 'stocks',
+@table_owner = 'production';
